648a594c9dd6975643f020f84721926b12a7e9b5
Shantanu Sutar <shantanusutar17@gmail.com>
2023-04-25 09:53:03 +0530

    fetching data from rest countries api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MdOutlineLightMode } from "react-icons/md";
 import "./App.css";
+import Country from "./Country";
+
+const url = "https://restcountries.com/v3.1/all";
+
 const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  let response = [];
+  const fetchData = async () => {
+    response = await fetch(url).then((res) => {
+      return res.json();
+    });
+
+    console.log(response);
+    setCountries(response);
+  };
+
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div className="container">
       <header>
@@ -11,6 +35,7 @@ const App = () => {
           Light
         </button>
       </header>
+      <Country />
     </div>
   );
 };
--- src/Country.jsx
+++ src/Country.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Country = () => {
-  return <div>Country</div>;
+  return (
+    <div className="country">
+      <picture>
+        <img src="" alt="" />
+      </picture>
+      <section>
+        <h2>name</h2>
+        <p>
+          <span className="country-info-highlight">Population:</span>
+          population
+        </p>
+        <p>
+          <span className="country-info-highlight"> Capital: </span>
+          capital
+        </p>
+        <p>
+          <span className="country-info-highlight">Region: </span> region
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default Country;
