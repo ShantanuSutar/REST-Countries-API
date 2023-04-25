6a04ed69dd2751f4b668f2569fe6e176ae507279
Shantanu Sutar <shantanusutar17@gmail.com>
2023-04-25 09:42:36 +0530

    header and its styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { MdOutlineLightMode } from "react-icons/md";
+import "./App.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <header>
+        <h1>Where in the world?</h1>
+        <button className="toggle">
+          <MdOutlineLightMode />
+          Light
+        </button>
+      </header>
+    </div>
+  );
 };
 
 export default App;
